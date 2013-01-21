f3646847afd63e91a1657cf4cef7f27c68174bd7
Alex Stomp <stompy1208@gmail.com>
2013-01-21 13:33:57 -0600

    first commit

---
+++ Gemfile
@@ -0,0 +1,21 @@
+source "http://bundler-api.herokuapp.com"
+
+gem 'rails', '3.2.8'
+
+gem 'pg'
+
+group :development, :test do
+  gem "nifty-generators"
+end
+
+group :assets do
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+	gem 'sass-rails',   '~> 3.2.3'
+end
+
+gem 'jquery-rails'
+
+gem "bcrypt-ruby", :require => "bcrypt"
+
+gem "mocha", :group => :test
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fantasia::Application.config.secret_token = '971039eb4fa76f32e63d072fff1eb8c2aec05d8d1033360e4fc15cb744585fd89f2760b2bc3115dedc2b7b03e63ae346155d2909a5dc54d69c322f91dd1981d4'
